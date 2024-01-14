8b7e0d18e7ccd3fddac4fd77abd3a7e5f1971c37
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-14 15:03:39 +0800

    refactor: use page-container innerHTML

--- projects/clr-extension/src/lib/components/page-container/page-container.component.spec.ts
+++ projects/clr-extension/src/lib/components/page-container/page-container.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PageContainerComponent } from './page-container.component';
+import {PageContainerComponent} from './page-container.component';
 
 describe('PageContainerComponent', () => {
   let component: PageContainerComponent;
@@ -8,10 +8,9 @@ describe('PageContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageContainerComponent]
-    })
-    .compileComponents();
-    
+      imports: [PageContainerComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PageContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
