94e362273c12fd00345fb0eac691e7bb0af6261f
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-15 14:02:20 +0800

    refactor: home page layout and common style

--- projects/demo-application/src/app/app.config.ts
+++ projects/demo-application/src/app/app.config.ts
@@ -1,9 +1,10 @@
 import {provideHttpClient} from '@angular/common/http';
 import {ApplicationConfig} from '@angular/core';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
 };
