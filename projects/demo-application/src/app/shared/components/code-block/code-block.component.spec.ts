f3a4eeb4dde6c4bf3897ea82d7590ec63464df01
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-15 15:34:16 +0800

    https://docs.netlify.com/routing/redirects/rewrites-proxies/?utm_content=ask-netlify#history-pushstate-and-single-page-apps chore: fix refresh SPA 404 referring

--- projects/demo-application/src/app/shared/components/code-block/code-block.component.spec.ts
+++ projects/demo-application/src/app/shared/components/code-block/code-block.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CodeBlockComponent } from './code-block.component';
+import {CodeBlockComponent} from './code-block.component';
 
 describe('CodeBlockComponent', () => {
   let component: CodeBlockComponent;
@@ -8,10 +8,9 @@ describe('CodeBlockComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CodeBlockComponent]
-    })
-    .compileComponents();
-    
+      imports: [CodeBlockComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CodeBlockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
