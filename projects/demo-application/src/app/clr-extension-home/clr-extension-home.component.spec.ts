42b3d50e0c301f4c86f7e7ff9ad167eeaeec0772
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-16 17:21:00 +0800

    refactor: refine home pages

---
+++ projects/demo-application/src/app/clr-extension-home/clr-extension-home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClrExtensionHomeComponent } from './clr-extension-home.component';
+
+describe('ClrExtensionHomeComponent', () => {
+  let component: ClrExtensionHomeComponent;
+  let fixture: ComponentFixture<ClrExtensionHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ClrExtensionHomeComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ClrExtensionHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/demo-application/src/app/ngx-extension-home/ngx-extension-home.component.ts
@@ -0,0 +1,16 @@
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ClarityModule} from '@clr/angular';
+import {PageContainerComponent} from 'clr-extension';
+
+import {CodeBlockComponent} from '../shared/components/code-block/code-block.component';
+
+@Component({
+  selector: 'app-ngx-extension-home',
+  standalone: true,
+  imports: [CommonModule, ClarityModule, PageContainerComponent, CodeBlockComponent],
+  templateUrl: './ngx-extension-home.component.html',
+  styleUrl: './ngx-extension-home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NgxExtensionHomeComponent {}
