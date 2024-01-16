2e42a28298ac77696f9bd02f3f31d9ab7a50fc85
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-16 17:21:00 +0800

    refactor: refine home pages

---
+++ projects/demo-application/src/app/ngx-extension-home/ngx-extension-home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgxExtensionHomeComponent } from './ngx-extension-home.component';
+
+describe('NgxExtensionHomeComponent', () => {
+  let component: NgxExtensionHomeComponent;
+  let fixture: ComponentFixture<NgxExtensionHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NgxExtensionHomeComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NgxExtensionHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
