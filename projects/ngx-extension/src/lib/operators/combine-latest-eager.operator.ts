5d950566e2430dca3b3c648ceb43eacac499e008
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-20 18:26:30 +0800

    feat: support force option for combine-latest-eager

--- projects/ngx-extension/src/lib/operators/combine-latest-eager.operator.ts
+++ projects/ngx-extension/src/lib/operators/combine-latest-eager.operator.ts
@@ -2,26 +2,35 @@ import {combineLatest, Observable, startWith, Subject} from 'rxjs';
 
 export function combineLatestEager<T extends Observable<unknown>[]>(
   sources: T,
+  force?: boolean,
 ): Observable<{[K in keyof T]: T[K] extends Observable<infer U> ? U | null : never}>;
 
 export function combineLatestEager<T extends Record<string, Observable<unknown>>>(
   sources: T,
+  force?: boolean,
 ): Observable<{[K in keyof T]: T[K] extends Observable<infer U> ? U | null : never}>;
 
 /**
- * Adds startWith(null) for each Subject in combineLatest.
- * @param sources observables to be passed in combineLatest
+ * Adds startWith(null) for each Subject in combineLatest when the second parameter force is false.
+ * When force is true, each observable will startWith null.
+ * @param sources Observables to be passed in combineLatest
+ * @param force If true, all source observables will startWith null
  * @returns combineLatest operator with Subject pipe(startWith(null))
  */
 export function combineLatestEager<T>(
   sources: Array<Observable<T>> | Record<string, Observable<T>>,
+  force = false,
 ): Observable<Array<T | null> | Record<string, T | null>> {
   if (Array.isArray(sources)) {
     // If sources is an array of observables
     return combineLatest(
       sources.map((obs$) => {
-        // Check if obs$ is a Subject, if true, apply startWith(null)
-        return obs$ instanceof Subject ? obs$.pipe(startWith(null)) : obs$;
+        if (force) {
+          return obs$.pipe(startWith(null));
+        } else {
+          // Check if obs$ is a Subject, if true, apply startWith(null)
+          return obs$ instanceof Subject ? obs$.pipe(startWith(null)) : obs$;
+        }
       }),
     );
   } else if (typeof sources === 'object' && sources !== null) {
@@ -29,7 +38,12 @@ export function combineLatestEager<T>(
     const observables: Record<string, Observable<T | null>> = {};
 
     for (const [key, value] of Object.entries(sources)) {
-      observables[key] = value instanceof Subject ? value.pipe(startWith(null)) : value;
+      if (force) {
+        observables[key] = value.pipe(startWith(null));
+      } else {
+        // Check if obs$ is a Subject, if true, apply startWith(null)
+        observables[key] = value instanceof Subject ? value.pipe(startWith(null)) : value;
+      }
     }
 
     return combineLatest(observables);
