41eee26859e22060c5372588525be96a2c7e6a44
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-23 14:47:39 +0800

    feat: add callout component and clr-datagrid util

---
+++ projects/demo-application/src/app/clr-lib/pages/clr-datagrid-util/clr-datagrid-util.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClrDatagridUtilComponent } from './clr-datagrid-util.component';
+
+describe('ClrDatagridUtilComponent', () => {
+  let component: ClrDatagridUtilComponent;
+  let fixture: ComponentFixture<ClrDatagridUtilComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ClrDatagridUtilComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ClrDatagridUtilComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/demo-application/src/app/clr-lib/pages/clr-datagrid-util/clr-datagrid-util.component.ts
@@ -0,0 +1,69 @@
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ClarityModule} from '@clr/angular';
+import {CalloutComponent, PageContainerComponent} from 'clr-extension';
+
+import {CodeBlockComponent} from '../../../shared/components/code-block/code-block.component';
+import {highlight} from '../../../shared/utils/highlight.util';
+
+@Component({
+  selector: 'app-clr-datagrid-util',
+  standalone: true,
+  imports: [CommonModule, ClarityModule, PageContainerComponent, CalloutComponent, CodeBlockComponent],
+  templateUrl: './clr-datagrid-util.component.html',
+  styleUrl: './clr-datagrid-util.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClrDatagridUtilComponent {
+  pageQueryModelCode = highlight(`
+export interface PageQuery {
+  /**
+   * The page number for pagination. Starts from 1.
+   */
+  page: number;
+
+  /**
+   * The number of items per page in the result set.
+   */
+  pageSize: number;
+
+  /**
+   * Optional: The field to sort in ascending order.
+   */
+  sortAsc?: string;
+
+  /**
+   * Optional: The field to sort in descending order.
+   */
+  sortDesc?: string;
+
+  /**
+   * Optional: The filter criteria in FIQL (Feed Item Query Language) format.
+   * Example: name==*term*;enabled==false
+   */
+  filter?: string;
+}
+`);
+
+  stateCode = highlight(`
+// Clarity Datagrid state example
+{
+  "page": {"from": 0, "to": 9, "size": 10, "current": 1},
+  "sort": {"by": "date", "reverse": false},
+  "filters": [
+    {"property": "name", "value": "mike"},
+    {"property": "job", "value": "programming"}
+  ]
+}
+  `);
+
+  outputCode = highlight(`
+// output data returned by convertToHttpParams
+{
+  "page": 1,
+  "pageSize": 10,
+  "filter": "name==*mike*;job==*programming*",
+  "sortAsc": "date"
+}
+    `);
+}
