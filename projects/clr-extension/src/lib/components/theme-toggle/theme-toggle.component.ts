00a5de9babcd60488fe895eaede04e102e018db3
Guanghui Wang <guanghui-wang@foxmail.com>
2024-01-31 18:36:07 +0800

    fix: use non-signal syntax

--- projects/clr-extension/src/lib/components/theme-toggle/theme-toggle.component.ts
+++ projects/clr-extension/src/lib/components/theme-toggle/theme-toggle.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, computed, EventEmitter, Input, OnInit, Output, signal} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ClarityIcons, moonIcon, sunIcon} from '@cds/core/icon';
 import {ClarityModule} from '@clr/angular';
 
@@ -14,48 +14,47 @@ ClarityIcons.addIcons(sunIcon);
   imports: [ClarityModule],
   templateUrl: './theme-toggle.component.html',
   styleUrl: './theme-toggle.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeToggleComponent implements OnInit {
   @Input() lang = '';
   @Output() changeTheme = new EventEmitter<string>();
 
-  hovering = signal(false);
-  theme = signal(localStorage['cds-theme'] || 'light');
+  hovering = false;
+  theme: 'dark' | 'light' = localStorage['cds-theme'] || 'light';
 
-  themeDisplayName = computed(() => {
+  get themeDisplayName() {
     return this.translationService.translate(
-      this.theme() === 'light' ? 'theme-toggle.dark' : 'theme-toggle.light',
+      this.theme === 'light' ? 'theme-toggle.dark' : 'theme-toggle.light',
       this.lang,
     );
-  });
+  }
 
-  ariaLabel = computed(() => {
+  get ariaLabel() {
     return this.translationService.translate(
-      this.theme() === 'light' ? 'theme-toggle.toggleDark' : 'theme-toggle.toggleLight',
+      this.theme === 'light' ? 'theme-toggle.toggleDark' : 'theme-toggle.toggleLight',
       this.lang,
     );
-  });
+  }
 
-  iconShape = computed(() => {
-    return this.theme() === 'light' ? 'moon' : 'sun';
-  });
+  get iconShape() {
+    return this.theme === 'light' ? 'moon' : 'sun';
+  }
 
   constructor(private translationService: TranslationService) {
     translationService.loadTranslationsForComponent('theme-toggle', themeToggleTranslations);
   }
 
   toggleTheme() {
-    const newTheme = this.theme() === 'light' ? 'dark' : 'light';
+    const newTheme = this.theme === 'light' ? 'dark' : 'light';
     this.setTheme(newTheme);
   }
 
   setHovering(value: boolean) {
-    this.hovering.set(value);
+    this.hovering = value;
   }
 
-  private setTheme(newTheme: string) {
-    this.theme.set(newTheme);
+  private setTheme(newTheme: 'dark' | 'light') {
+    this.theme = newTheme;
     localStorage['cds-theme'] = newTheme;
     document.body.setAttribute('cds-theme', newTheme);
     this.changeTheme.emit(newTheme);
