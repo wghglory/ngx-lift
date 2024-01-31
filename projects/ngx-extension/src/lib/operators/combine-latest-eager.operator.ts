5cfba17ad4b7d1d7cf9de1be13f8e2af7ed72243
KingMario <gcyyq@hotmail.com>
2024-01-31 23:11:37 +0800

    refactor the combine-latest-eager.operator by extracting function

--- projects/ngx-extension/src/lib/operators/combine-latest-eager.operator.ts
+++ projects/ngx-extension/src/lib/operators/combine-latest-eager.operator.ts
@@ -34,29 +34,24 @@ export function combineLatestEager<T>(
   sources: Array<Observable<T>> | Record<string, Observable<T>>,
   startWithNullForAll = false,
 ): Observable<Array<T | null> | Record<string, T | null>> {
+  function observableMapper<T>(observable: Observable<T>) {
+    if (startWithNullForAll) {
+      return observable.pipe(startWith(null));
+    } else {
+      // Check if observable is a Subject, if true, apply startWith(null)
+      return observable instanceof Subject ? observable.pipe(startWith(null)) : observable;
+    }
+  }
+
   if (Array.isArray(sources)) {
     // If sources is an array of observables
-    return combineLatest(
-      sources.map((obs$) => {
-        if (startWithNullForAll) {
-          return obs$.pipe(startWith(null));
-        } else {
-          // Check if obs$ is a Subject, if true, apply startWith(null)
-          return obs$ instanceof Subject ? obs$.pipe(startWith(null)) : obs$;
-        }
-      }),
-    );
+    return combineLatest(sources.map(observableMapper));
   } else if (typeof sources === 'object' && sources !== null) {
     // If sources is a dictionary of observables
     const observables: Record<string, Observable<T | null>> = {};
 
     for (const [key, value] of Object.entries(sources)) {
-      if (startWithNullForAll) {
-        observables[key] = value.pipe(startWith(null));
-      } else {
-        // Check if obs$ is a Subject, if true, apply startWith(null)
-        observables[key] = value instanceof Subject ? value.pipe(startWith(null)) : value;
-      }
+      observables[key] = observableMapper(value);
     }
 
     return combineLatest(observables);
