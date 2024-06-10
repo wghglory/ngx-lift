9d60a9fb1ad97ae331c2fd920ff441c0ad26f859
Derek Wang <guanghui-wang@foxmail.com>
2024-06-10 15:27:54 +0800

    refactor: enhance types of injectParams

--- projects/demo-application/src/app/ngx-lib/pages/signals/inject-params/inject-params.component.ts
+++ projects/demo-application/src/app/ngx-lib/pages/signals/inject-params/inject-params.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, effect} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, numberAttribute, Signal} from '@angular/core';
 import {ClarityModule} from '@clr/angular';
 import {CalloutComponent, PageContainerComponent} from 'clr-lift';
 import {injectParams} from 'ngx-lift';
@@ -15,32 +15,53 @@ import {highlight} from '../../../../shared/utils/highlight.util';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InjectParamsComponent {
+  // returns a signal with the current route params
   params = injectParams();
 
-  userId = injectParams('id'); // returns a signal with the value of the id param
+  // returns a signal with the keys of the params
+  paramsKeys = injectParams((params) => Object.keys(params));
 
-  paramsKeys = injectParams((params) => Object.keys(params)); // returns a signal with the keys of the params
+  // returns a signal with the value of the id param
+  userId: Signal<string | null> = injectParams('id');
+
+  // returns a signal with the value of the id param, initialValue is 1
+  id: Signal<number> = injectParams('id', {transform: numberAttribute, initialValue: 1});
+
+  // pass a transform function directly
+  idByTransformFn = injectParams((params) => params['id'] as string);
 
   constructor() {
     effect(() => {
       console.log(this.params(), 'params');
-      console.log(this.userId(), 'userId');
       console.log(this.paramsKeys(), 'paramsKeys');
+      console.log(this.userId(), 'userId');
+      console.log(this.id(), 'id');
+      console.log(this.idByTransformFn(), 'idByTransformFn');
     });
   }
 
   code = highlight(`
+import {numberAttribute, Signal} from '@angular/core';
 import {injectParams} from 'ngx-lift';
 
 export class InjectParamsComponent {
   // returns a signal with the current route params
   params = injectParams();
 
-  // returns a signal with the value of the id param
-  userId = injectParams('id');
-
   // returns a signal with the keys of the params
   paramsKeys = injectParams((params) => Object.keys(params));
+
+  // returns a signal with the value of the id param
+  userId: Signal<string | null> = injectParams('id');
+
+  // returns a signal with the value of the id param, initialValue is 1
+  id: Signal<number> = injectParams('id', {transform: numberAttribute, initialValue: 1})
+
+  // pass a transform function directly
+  name = injectParams((params) => params['name'] as string);
+
+  // whenever userId param is changed, fetch the user
+  user = computedAsync(() => this.userService.getUser(this.userId()));
 }
     `);
 }
--- projects/ngx-lift/src/lib/signals/inject-params.ts
+++ projects/ngx-lift/src/lib/signals/inject-params.ts
@@ -3,6 +3,28 @@ import {toSignal} from '@angular/core/rxjs-interop';
 import {ActivatedRoute, type Params} from '@angular/router';
 import {map} from 'rxjs';
 
+type ParamsTransformFn<Output> = (params: Params) => Output;
+
+/**
+ * The `InputOptions` interface defines options for configuring the behavior of the `injectParams` function.
+ *
+ * @template Output - The expected type of the read value.
+ */
+export interface ParamsOptions<Output> {
+  /**
+   * A transformation function to convert the written value to the expected read value.
+   *
+   * @param v - The value to transform.
+   * @returns The transformed value.
+   */
+  transform?: (v: string) => Output;
+
+  /**
+   * The initial value to use if the parameter is not present or undefined.
+   */
+  initialValue?: Output;
+}
+
 /**
  * Injects the params from the current route.
  * If a key is provided, returns the value of that key.
@@ -10,26 +32,86 @@ import {map} from 'rxjs';
  * Otherwise, returns the entire params object.
  *
  * @example
+ * const params = injectParams(); // Returns the entire params object
  * const userId = injectParams('id'); // Returns the value of the 'id' param
  * const userId = injectParams(p => p['id'] as string); // Returns the 'id' param using a custom transform function
- * const params = injectParams(); // Returns the entire params object
+ * const userId = injectParams('id', { transform: numberAttribute, initialValue: 1 });
  *
- * @param keyOrTransform OPTIONAL The key of the param to return, or a transform function to apply to the params object
+ * @param keyOrParamsTransform OPTIONAL The key of the param to return, or a transform function to apply to the params object
  */
 export function injectParams(): Signal<Params>;
+
+export function injectParams<Output>(fn: ParamsTransformFn<Output>): Signal<Output>;
+
 export function injectParams(key: string): Signal<string | null>;
-export function injectParams<T>(transform: (params: Params) => T): Signal<T>;
-export function injectParams<T>(keyOrTransform?: string | ((params: Params) => T)): Signal<T | Params | string | null> {
+
+// for boolean or number, if initialValue is provided, transform is a must
+export function injectParams(
+  key: string,
+  options: {transform: (v: string) => boolean; initialValue: boolean},
+): Signal<boolean>;
+export function injectParams(
+  key: string,
+  options: {transform: (v: string) => number; initialValue: number},
+): Signal<number>;
+// for string, transform is optional
+export function injectParams(
+  key: string,
+  options: {transform?: (v: string) => string; initialValue: string},
+): Signal<string>;
+
+// initialValue not provided, must provide transform fn
+export function injectParams(
+  key: string,
+  options: {transform: (v: string) => boolean; initialValue?: undefined},
+): Signal<boolean | null>;
+export function injectParams(
+  key: string,
+  options: {transform: (v: string) => number; initialValue?: undefined},
+): Signal<number | null>;
+export function injectParams(
+  key: string,
+  options: {transform: (v: string) => string; initialValue?: undefined},
+): Signal<string | null>;
+
+export function injectParams<Output>(
+  keyOrParamsTransform?: string | ParamsTransformFn<Output>,
+  options: ParamsOptions<Output> = {},
+): Signal<Output | Params | string | boolean | number | null> {
   assertInInjectionContext(injectParams);
 
   const route = inject(ActivatedRoute);
-  const params = route.snapshot.params;
+  const initialParams = route.snapshot.params;
 
-  if (typeof keyOrTransform === 'function') {
-    return toSignal(route.params.pipe(map(keyOrTransform)), {initialValue: keyOrTransform(params)});
+  const {transform, initialValue} = options;
+
+  // injectParams(): Signal<Params>
+  if (!keyOrParamsTransform) {
+    return toSignal(route.params, {initialValue: initialParams});
   }
 
-  const getParam = (params: Params) => (keyOrTransform ? params?.[keyOrTransform] ?? null : params);
+  // injectParams<Output>(fn: ParamsTransformFn<Output>): Signal<Output>
+  if (typeof keyOrParamsTransform === 'function') {
+    return toSignal(route.params.pipe(map(keyOrParamsTransform)), {initialValue: keyOrParamsTransform(initialParams)});
+  }
 
-  return toSignal(route.params.pipe(map(getParam)), {initialValue: getParam(params)});
+  // keyOrParamsTransform is string.
+  // export function injectParams(key: string): Signal<string | null>;
+  // export function injectParams(key: string, options: { transform: (v: string) => boolean; initialValue: boolean }): Signal<boolean>;
+  // export function injectParams(key: string, options: { transform: (v: string) => number; initialValue: number }): Signal<number>;
+  // export function injectParams(key: string, options: { transform?: (v: string) => string; initialValue: string }): Signal<string>;
+  // export function injectParams(key: string, options: { transform: (v: string) => boolean; initialValue?: undefined }): Signal<boolean | null>;
+  // export function injectParams(key: string, options: { transform: (v: string) => number; initialValue?: undefined }): Signal<number | null>;
+  // export function injectParams(key: string, options: { transform: (v: string) => string; initialValue?: undefined }): Signal<string | null>;
+  const getParam = (params: Params) => {
+    const param = params?.[keyOrParamsTransform] as string | undefined;
+
+    if (!param) {
+      return initialValue ?? null;
+    }
+
+    return transform ? transform(param) : param;
+  };
+
+  return toSignal(route.params.pipe(map(getParam)), {initialValue: getParam(initialParams)});
 }
