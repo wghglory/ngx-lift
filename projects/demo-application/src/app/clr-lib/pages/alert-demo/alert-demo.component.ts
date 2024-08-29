01150041f9d2b9cbc577bacc0838867924f3fcb9
Guanghui Wang <guanghui-wang@foxmail.com>
2024-08-29 14:25:28 +0800

    feat(clr-lift): enhance alert component with content projection

--- projects/demo-application/src/app/clr-lib/pages/alert-demo/alert-demo.component.ts
+++ projects/demo-application/src/app/clr-lib/pages/alert-demo/alert-demo.component.ts
@@ -1,7 +1,7 @@
 import {HttpErrorResponse} from '@angular/common/http';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ClarityModule} from '@clr/angular';
-import {AlertComponent, PageContainerComponent} from 'clr-lift';
+import {AlertComponent, PageContainerComponent, TooltipModule} from 'clr-lift';
 
 import {highlight} from '../../../shared/utils/highlight.util';
 import {CodeBlockComponent} from './../../../shared/components/code-block/code-block.component';
@@ -9,7 +9,7 @@ import {CodeBlockComponent} from './../../../shared/components/code-block/code-b
 @Component({
   selector: 'app-alert-demo',
   standalone: true,
-  imports: [AlertComponent, PageContainerComponent, CodeBlockComponent, ClarityModule],
+  imports: [AlertComponent, PageContainerComponent, TooltipModule, CodeBlockComponent, ClarityModule],
   templateUrl: './alert-demo.component.html',
   styleUrl: './alert-demo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,13 +19,40 @@ export class AlertDemoComponent {
     error: {message: 'angular http error response'},
   });
 
-  standardAlertsCode = highlight(`
+  standardApiErrorCode = highlight(`
 <!-- error is instance of HttpErrorResponse -->
 <cll-alert [error]="error" />
+  `);
 
+  standardContentCode = highlight(`
 <cll-alert alertType="success" content="I'm a successful content" />
 <cll-alert alertType="info" content="I'm a info content" />
 <cll-alert alertType="warning" content="I'm a warning content" />
+<cll-alert alertType="danger" content="I'm a danger content" />
+
+<!-- Pass HTML tags -->
+<cll-alert alertType="info" [content]="'<strong>Hello world</strong>, I like clr-lift!'" />
+  `);
+
+  standardProjectionCode = highlight(`
+<cll-alert>
+  <p class="!mt-0 !mb-2">Multiple errors are listed as below by <strong>content projection</strong>.</p>
+  <ol class="list-decimal">
+    <li>Cannot read undefined.</li>
+    <li>
+      Cannot upload a object.
+      <button
+        [cllTooltip]="'server is down'"
+        [cllTooltipPosition]="'tooltip-right'"
+        class="btn btn-sm btn-link btn-icon !my-0 !px-0"
+        aria-label="info"
+        type="button"
+      >
+        <cds-icon shape="info-circle" />
+      </button>
+    </li>
+  </ol>
+</cll-alert>
   `);
 
   standardSmallAlertCode = highlight(`
@@ -49,6 +76,10 @@ export class AlertDemoComponent {
   content="I'm a successful content <a href='javascript:void(0)' class='btn btn-outline btn-sm'>View</a>"
   [isAppLevel]="true"
 />
+<cll-alert alertType="success" [isAppLevel]="true">
+  I'm a successful content injected by content projection &nbsp;
+  <a href="javascript:void(0)" class="btn btn-inverse btn-sm">View</a>
+</cll-alert>
 <cll-alert alertType="info" content="I'm a info content" [isAppLevel]="true" />
 <cll-alert alertType="warning" content="I'm a warning content" [isAppLevel]="true" />
     `);
