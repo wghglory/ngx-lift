f6f081673d94ee8447440914c1ce407da9adbaae
Guanghui Wang <guanghui-wang@foxmail.com>
2024-09-04 11:09:08 +0800

    feat: registerLocaleData

--- projects/demo-application/src/app/app.config.ts
+++ projects/demo-application/src/app/app.config.ts
@@ -1,4 +1,15 @@
+import {registerLocaleData} from '@angular/common';
 import {provideHttpClient} from '@angular/common/http';
+import localeDe from '@angular/common/locales/de';
+import localeEn from '@angular/common/locales/en';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import localeIt from '@angular/common/locales/it';
+import localeJa from '@angular/common/locales/ja';
+import localeKo from '@angular/common/locales/ko';
+import localePt from '@angular/common/locales/pt';
+import localeRu from '@angular/common/locales/ru';
+import localeZh from '@angular/common/locales/zh';
 import {ApplicationConfig, LOCALE_ID} from '@angular/core';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
@@ -7,6 +18,10 @@ import player from 'lottie-web';
 import {provideLottieOptions} from 'ngx-lottie';
 
 import {routes} from './app.routes';
+
+[localeEn, localeFr, localeDe, localeEs, localeIt, localePt, localeZh, localeJa, localeKo, localeRu].forEach((locale) =>
+  registerLocaleData(locale),
+);
 
 export const appConfig: ApplicationConfig = {
   providers: [
