b3412df6fbf81323badb5f8481ae55465078790e
Derek Wang <guanghui-wang@foxmail.com>
2024-10-31 14:03:05 +0800

    refactor(ngx-lift): poll operator param (#76)

--- projects/ngx-lift/src/lib/operators/poll.operator.ts
+++ projects/ngx-lift/src/lib/operators/poll.operator.ts
@@ -27,8 +27,8 @@ import {isPromise} from '../utils/is-promise.util';
  * @param {object} options - The configuration options for polling.
  * @param {number} options.interval - The interval in milliseconds between each poll.
  * @param {(params: any) => Observable<Data> | Data} options.pollingFn - A function that returns an Observable, Promise, or primitive value.
- * @param {Observable<Input> | Signal<Input>} [options.trigger] - An optional Observable or Signal that triggers a manual poll.
- * @param {(input: Input) => any} [options.paramsBuilder] - An optional function that builds parameters for the polling function based on the input. The value emitted by the trigger observable will serve as the parameter.
+ * @param {Observable<Input> | Signal<Input>} [options.forceRefresh] - An optional Observable or Signal that triggers a manual refresh of the polling function.
+ * @param {(input: Input) => any} [options.paramsBuilder] - An optional function that builds parameters for the polling function based on the input. The value emitted by the forceRefresh observable will serve as the parameter.
  * @returns {Observable<AsyncState<Data>>} An Observable emitting objects representing the state of the asynchronous operation.
  */
 export function poll<Data>(options: {
@@ -36,56 +36,56 @@ export function poll<Data>(options: {
   pollingFn: (params: any) => Observable<Data> | Promise<Data> | Data;
 }): Observable<AsyncState<Data>>;
 
-// trigger output is the pollingFn params' input
+// forceRefresh output is the pollingFn params' input
 export function poll<Data, Input>(options: {
   interval: number;
   pollingFn: (params: Input) => Observable<Data> | Promise<Data> | Data;
-  trigger: Observable<Input> | Signal<Input>;
+  forceRefresh: Observable<Input> | Signal<Input>;
 }): Observable<AsyncState<Data>>;
 
-// paramsBuilder exists, trigger output is the paramsBuilder params' input
+// paramsBuilder exists, forceRefresh output is the paramsBuilder params' input
 export function poll<Data, Input>(options: {
   interval: number;
   pollingFn: (params: any) => Observable<Data> | Promise<Data> | Data;
-  trigger: Observable<Input> | Signal<Input>;
+  forceRefresh: Observable<Input> | Signal<Input>;
   paramsBuilder: (input: Input) => any;
 }): Observable<AsyncState<Data>>;
 
 export function poll<Data, Input>(options: {
   interval: number;
   pollingFn: (params: any) => Observable<Data> | Promise<Data> | Data;
-  trigger?: Observable<Input> | Signal<Input>;
+  forceRefresh?: Observable<Input> | Signal<Input>;
   paramsBuilder?: (input: Input) => any;
 }): Observable<AsyncState<Data>> {
   const timerEmitValue = '__timer__emission__';
   const timer$ = timer(0, options.interval).pipe(map((i) => `${timerEmitValue}${i}`));
 
   const trigger$ =
-    options.trigger === undefined
+    options.forceRefresh === undefined
       ? EMPTY
-      : isObservable(options.trigger)
-        ? options.trigger
-        : toObservable(options.trigger);
+      : isObservable(options.forceRefresh)
+        ? options.forceRefresh
+        : toObservable(options.forceRefresh);
 
-  let inputByTrigger: Input | undefined = undefined; // if trigger is not provided, input will be undefined
+  let inputByForceRefresh: Input | undefined = undefined; // if forceRefresh is not provided, input will be undefined
 
   return merge(trigger$, timer$).pipe(
     exhaustMap((input) => {
-      // input can be either by trigger or timer
+      // input can be either by forceRefresh or timer
       const isTimerTrigger = typeof input === 'string' && input.includes(timerEmitValue);
       const isManualTrigger = !isTimerTrigger;
       if (isManualTrigger) {
-        inputByTrigger = input as Input;
+        inputByForceRefresh = input as Input;
       }
 
-      // build params by trigger input
+      // build params by input
       // if paramsBuilder is provided, params will be the value of this function call
-      // if paramsBuilder is not provided, params will be the value emitted by the trigger
-      const params = options.paramsBuilder ? options.paramsBuilder(inputByTrigger as Input) : inputByTrigger;
+      // if paramsBuilder is not provided, params will be the value emitted by the forceRefresh
+      const params = options.paramsBuilder ? options.paramsBuilder(inputByForceRefresh as Input) : inputByForceRefresh;
 
-      // NOTE: using exhaustMap will NOT emit ${timerEmitValue}0 if trigger is not provided
-      // using concatMap will emit ${timerEmitValue}0 if trigger is not provided
-      const isFirstRequest = input === `${timerEmitValue}0`; // timer first emission when trigger is not provided
+      // NOTE: using exhaustMap will NOT emit ${timerEmitValue}0 if forceRefresh is not provided
+      // using concatMap will emit ${timerEmitValue}0 if forceRefresh is not provided
+      const isFirstRequest = input === `${timerEmitValue}0`; // timer first emission when forceRefresh is not provided
       const shouldShowLoading = isManualTrigger || isFirstRequest;
 
       const fnResult = options.pollingFn(params);
